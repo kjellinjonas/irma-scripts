ea5f790846b1619aa1b3f25d9dfd428e7674cc11
Jonas Kjellin <jonas.kjellin@gmail.com>
2025-04-01 09:50:25 +0200

    updates after review

--- stage_bpa.py
+++ stage_bpa.py
@@ -1,13 +1,25 @@
-#!/usr/bin//env python
+#!/usr/bin/env python
 
 import argparse
-import os
+import logging
 import shutil
-import glob
 import subprocess
+from datetime import datetime
+from pathlib import Path
+
 
 class Stager:
-    def __init__(self, project, pipeline, analysis_dir, results_dir, stage_dir, config, wes=False, force=False):
+    def __init__(
+        self,
+        project,
+        pipeline,
+        analysis_dir,
+        results_dir,
+        stage_dir,
+        config,
+        wes=False,
+        force=False,
+    ):
         self.project = project
         self.pipeline = pipeline
         self.force = force
@@ -16,38 +28,92 @@ class Stager:
         self.analysis_dir = analysis_dir
         self.stage_dir = stage_dir
         self.results_dir = results_dir
-        self.readme = os.path.join(config["readme_dir"], self._get_readme())
+        self.readme = Path(config["readme_dir"]) / self._get_readme()
 
     def run(self):
         try:
             self._check_analysis()
             if self._check_staging() and not self.force:
-                print(f"Project {self.project} already staged. Use --force to overwrite.")
+                logging.info(
+                    f"Project {self.project} already staged. Use --force to overwrite."
+                )
+                return 0
+
+            if not self._check_file_status():
                 return 0
 
             self._prepare_for_staging()
             self._stage_files()
             self._calculate_checksums()
-            delivery_readme = os.path.join(self.stage_dir, os.path.basename(self.readme))
-            print(f"Project {self.project} staged successfully!\n")
-            print("Before delivery:")
-            print(f"Eensure that there are no errors in {self.config['checksums_log']}")
-            print(f"Make sure that information in {delivery_readme} is correct.")
-            print(f"  Note. Remember to update cheksum for README if you make any changes.\n")
-            print(f"Make README available to the project coordinators:")
-            print(f"  cp {delivery_readme} /proj/ngi2016001/incoming/reports/{self.project}/\n")
+            delivery_readme = Path(self.stage_dir) / self.readme.name
+            logging.info(f"\nProject {self.project} staged successfully!\n"
+                          "Before delivery:\n"
+                         f"Ensure that there are no errors in {self.config['checksums_log']}\n"
+                         f"Make sure that information in {delivery_readme} is correct.\n"
+                          "  Note. Remember to update checksum for README if you make any changes.\n"
+                         f"Make README available to the project coordinators:\n"
+                         f"  cp {delivery_readme} /proj/ngi2016001/incoming/reports/{self.project}/\n"
+                     )
             return 0
 
         except Exception as e:
-            print(f"Error: {e}")
-            return 1 
+            logging.error(f"Error: {e}")
+            return 1
 
     def _check_analysis(self):
-        if not os.path.exists(self.results_dir):
-            raise FileNotFoundError(f"results directory not found: {self.analysis_dir}")
+        if not self.results_dir.exists():
+            logging.error(f"results directory not found: {self.analysis_dir}")
+            raise FileNotFoundError()
 
     def _check_staging(self):
-        return os.path.exists(self.stage_dir)
+        return self.stage_dir.exists()
+
+    def _check_file_status(self):
+        """List files to be deleted during staging and prompt user for confirmation"""
+        to_b_del = []
+
+        if self.force and self.stage_dir.exists():
+            to_b_del.append((self.stage_dir, "- Overwritten with new staging."))
+
+        match self.pipeline:
+            case "sarek":
+                sarek_files = list(self.config["sarek_multiqc"].glob("*"))
+                if len(sarek_files) != 0:
+                    for f in sarek_files:
+                        to_b_del.append(
+                            (f, "- Exchanged to custom generated MultiQC files")
+                        )
+                else:
+                    logging.info(f"No files found in {self.config['sarek_multiqc']}")
+            case "rnaseq" | "methylseq":
+                qualimap_dir = self.config[f"{self.pipeline}_qualimap"]
+                if qualimap_dir.exists():
+                    to_b_del.append(
+                        (qualimap_dir, "- Compressed (.tar.gz), original removed.")
+                    )
+
+        if len(to_b_del) == 0:
+            logging.info(
+                "No files/directories will be modified or deleted during staging."
+            )
+            return True
+
+        logging.info(
+            "The following files/directories will be modified or deleted during staging:"
+        )
+        for item, reason in to_b_del:
+            item_type = "directory" if item.is_dir() else "file"
+            logging.info(f"{item} ({item_type}, {reason})")
+
+        while True:
+            response = input("Do you want to proceed with staging? (y/n): ").lower()
+            if response == "y":
+                return True
+            elif response == "n":
+                logging.info("Staging aborted.")
+                return False
+            else:
+                logging.warning("Invalid input. Please enter 'y' or 'n'.")
 
     def _prepare_for_staging(self):
         match self.pipeline:
@@ -64,24 +130,30 @@ class Stager:
         """
         try:
             # move markduplicate files from results to analysis dir
-            if os.path.exists(self.config["md_src"]):
-                shutil.move(self.config["md_src"], self.config["md_dest"])
+            if self.config["md_src"].exists():
+                shutil.move(str(self.config["md_src"]), str(self.config["md_dest"]))
 
-            if not os.path.exists(self.config["custom_report"]):
-                print(f"Custom multiQC report not found in {self.config['custom_report']}. Staging with pipeline generated report.")
+            if not self.config["custom_report"].exists():
+                logging.info(
+                    f"Custom multiQC report not found in {self.config['custom_report']}. Staging with pipeline generated report."
+                )
                 return
-            
+
             # Replace pipeline generated multiQC with custom
-            
-            for f in glob.glob(os.path.join(self.config["sarek_multiqc"], "*")):
-                os.remove(f)
-            report_replc = os.path.join(self.config["sarek_multiqc"], os.path.basename(self.config["custom_report"]))
-            data_replc = os.path.join(self.config["sarek_multiqc"], os.path.basename(self.config["custom_data"]))
-            shutil.copy(self.config["custom_report"], report_replc)
-            shutil.copy(self.config["custom_data"], data_replc)
-            
+            # Double check behaviour if folder is empty
+            for f in self.config["sarek_multiqc"].glob("*"):
+                f.unlink()
+
+            report_replc = (
+                self.config["sarek_multiqc"] / self.config["custom_report"].name
+            )
+            data_replc = self.config["sarek_multiqc"] / self.config["custom_data"].name
+            shutil.copy(str(self.config["custom_report"]), str(report_replc))
+            shutil.copy(str(self.config["custom_data"]), str(data_replc))
+
         except Exception as e:
-            raise RuntimeError(f"Sarek preparation failed: {e}")
+            logging.error(f"Sarek preparation failed: {e}")
+            raise RuntimeError()
 
     def _tar_qualimap(self):
         """
@@ -91,13 +163,19 @@ class Stager:
         """
         try:
             qualimap_dir = self.config[f"{self.pipeline}_qualimap"]
-            if os.path.exists(qualimap_dir):
-                tar_command = ["tar", "-czvf", f"{qualimap_dir}.tar.gz", qualimap_dir]
+            if qualimap_dir.exists():
+                tar_command = [
+                    "tar",
+                    "-czvf",
+                    f"{qualimap_dir}.tar.gz",
+                    str(qualimap_dir),
+                ]
                 subprocess.run(tar_command, check=True)
-                shutil.rmtree(qualimap_dir)
-        
+                shutil.rmtree(str(qualimap_dir))
+
         except Exception as e:
-            raise RuntimeError(f"{self.pipeline} prerparation failed: {e}")
+            logging.error(f"{self.pipeline} prerparation failed: {e}")
+            raise RuntimeError()
 
     def _stage_files(self):
         """
@@ -105,15 +183,23 @@ class Stager:
           - Copy pipeline specific README
           - Symlink project result dir
         """
-        if self.force and os.path.exists(self.stage_dir):
-            shutil.rmtree(self.stage_dir)
-        os.makedirs(self.stage_dir)
+        if self.force and self.stage_dir.exists():
+            shutil.rmtree(str(self.stage_dir))
+
+        if not self.stage_dir.parent.exists():
+            logging.error(
+                f"Parent directory of stage directory does not exist: {self.stage_dir.parent}"
+            )
+            raise FileNotFoundError()
+
+        self.stage_dir.mkdir()
         try:
-            shutil.copy(self.readme, self.stage_dir)
-            folder_name = os.path.basename(self.results_dir)
-            os.symlink(self.results_dir, os.path.join(self.stage_dir, folder_name))
+            shutil.copy(str(self.readme), str(self.stage_dir))
+            link_dest = self.stage_dir / self.results_dir.name
+            link_dest.symlink_to(str(self.results_dir))
         except Exception as e:
-            raise RuntimeError(f"Staging failed: {e}")
+            logging.error(f"Staging failed: {e}")
+            raise RuntimeError()
 
     def _get_readme(self):
         match self.pipeline:
@@ -127,7 +213,6 @@ class Stager:
                 else:
                     return self.config["readme_wgs"]
 
-
     def _calculate_checksums(self):
         """
         Submits a Slurm job to calculate checksums.
@@ -139,11 +224,16 @@ class Stager:
         try:
             sbatch_command = [
                 "sbatch",
-                "-A", self.config["slurm_account"],
-                "-n", self.config["slurm_nodes"],
-                "-t", self.config["slurm_time" ],
-                "-J", f"checksums_{self.project}",
-                "-o", self.config["checksums_log"],
+                "-A",
+                self.config["slurm_account"],
+                "-n",
+                self.config["slurm_nodes"],
+                "-t",
+                self.config["slurm_time"],
+                "-J",
+                f"checksums_{self.project}",
+                "-o",
+                self.config["checksums_log"],
                 "--wrap",
                 (
                     f"find -L {self.stage_dir} -not -type d -not -name 'checksums.md5' | "
@@ -151,67 +241,108 @@ class Stager:
                 ),
             ]
 
-            result = subprocess.run(sbatch_command, capture_output=True, text=True, check=True)
+            result = subprocess.run(
+                sbatch_command, capture_output=True, text=True, check=True
+            )
 
-            print(result.stdout)
+            logging.info(result.stdout)
 
         except subprocess.CalledProcessError as e:
-            raise RuntimeError(f"Slurm job submission failed: {e}")
+            logging.error(f"Slurm job submission failed: {e}")
+            raise RuntimeError()
         except Exception as e:
-            raise RuntimeError(f"An error occurred: {e}")
+            logging.error(f"An error occurred: {e}")
+            raise RuntimeError()
 
+
+def logging_setup(analysis_dir, project):
+    """Set up logging to both file and stdout"""
+    log_dir = Path(analysis_dir) / "logs"
+    log_dir.mkdir(exist_ok=True)
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    log_file = log_dir / f"{project}_staging_{timestamp}.log"
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        handlers=[logging.FileHandler(log_file), logging.StreamHandler()],
+    )
+
+def log_setup_info(project, pipeline, wes, force, analysis_dir, stage_dir, results_dir, config):
+    """Logs the script setup information."""
+    conf = "\n".join([f"{k}: {config[k]}" for k in config])
+    logging.info(f"\n\nStaging log for {project}"
+                  "\n\n### Parameters ###\n"
+                 f"Project: {project}\n"
+                 f"Pipeline: {pipeline}\n"
+                 f"WES: {wes}\n"
+                 f"Force: {force}\n"
+                 f"Analysis Directory: {analysis_dir}\n"
+                 f"Stage Directory: {stage_dir}\n"
+                 f"Results Directory: {results_dir}\n"
+                  "\n### Config ###\n"
+                 f"{conf}\n\n")
 
 def main():
     parser = argparse.ArgumentParser(
         description="Stage BPA results for delivery.",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,)
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
     parser.add_argument(
         "--pipeline",
         required=True,
         choices=["sarek", "rnaseq", "methylseq"],
-        help="Pipeline used for analysis.")
+        help="Pipeline used for analysis.",
+    )
     parser.add_argument(
-            "--project",
-            required=True,
-            help="Name of project to be staged for delivery")
+        "--project", required=True, help="Name of project to be staged for delivery"
+    )
     parser.add_argument(
-            "--wes",
-            action="store_true",
-            help="Stage WES analysis. Requires --pipeline=sarek")
-    parser.add_argument(
-        "--force",
+        "--wes",
         action="store_true",
-        help="Overwrite previous staging.")
+        help="Stage WES analysis. Requires --pipeline=sarek",
+    )
+    parser.add_argument(
+        "--force", action="store_true", help="Overwrite previous staging."
+    )
     parser.add_argument(
         "--analysis_path",
         default="/proj/ngi2016001/nobackup/NGI/ANALYSIS",
-        help="Path to where project folder to be staged is located.")
+        help="Path to where project folder to be staged is located.",
+    )
     parser.add_argument(
         "--delivery_path",
         default="/proj/ngi2016001/nobackup/NGI/DELIVERY",
-        help="Path to where staging should be performed")
+        help="Path to where staging should be performed",
+    )
     args = parser.parse_args()
 
     project = args.project
     pipeline = args.pipeline
     wes = args.wes
     force = args.force
-    analysis_dir = os.path.join(args.analysis_path, project)
-    stage_dir = os.path.join(args.delivery_path, project)
-    results_dir = os.path.join(analysis_dir, "results")
+    analysis_dir = Path(args.analysis_path) / project
+    stage_dir = Path(args.delivery_path) / project
+    results_dir = Path(analysis_dir) / "results"
 
     config = {
-        "md_src": os.path.join(results_dir, "preprocessing/markduplicates"),
-        "md_dest": os.path.join(analysis_dir, "markduplicates"),
-        "sarek_multiqc": os.path.join(results_dir, "reports/multiqc"),
-        "custom_report": os.path.join(analysis_dir, f"multiqc_ngi/{project}_multiqc_report.html"),
-        "custom_data": os.path.join(analysis_dir, f"multiqc_ngi/{project}_multiqc_report_data.zip"),
-        "rnaseq_qualimap": os.path.join(results_dir, "star_salmon/qualimap"),
-        "methylseq_qualimap": os.path.join(results_dir, "qualimap"),
-        "checksums_log": os.path.join(analysis_dir, "logs/checksums.log"),
-        "checksums": os.path.join(stage_dir, "checksums.md5"),
+        "md_src": Path(results_dir) / "preprocessing" / "markduplicates",
+        "md_dest": Path(analysis_dir) / "markduplicates",
+        "sarek_multiqc": Path(results_dir) / "reports" / "multiqc",
+        "custom_report": Path(analysis_dir)
+        / "multiqc_ngi"
+        / f"{project}_multiqc_report.html",
+        "custom_data": Path(analysis_dir)
+        / "multiqc_ngi"
+        / f"{project}_multiqc_report_data.zip",
+        "rnaseq_qualimap": Path(results_dir) / "star_salmon" / "qualimap",
+        "methylseq_qualimap": Path(results_dir) / "qualimap",
+        "checksums_log": Path(analysis_dir) / "logs" / "checksums.log",
+        "checksums": Path(stage_dir) / "checksums.md5",
         "slurm_account": "ngi2016001",
-        "slurm_nodes": "8",
+        "slurm_nodes": "2",
         "slurm_time": "10:00:00",
         "readme_dir": "/proj/ngi2016001/nobackup/NGI/softlinks",
         "readme_rna": "DELIVERY.README.RNASEQ.md",
@@ -220,9 +351,17 @@ def main():
         "readme_wes": "DELIVERY.README.SAREK.WES.md",
     }
 
-    stager = Stager(project, pipeline, analysis_dir, results_dir, stage_dir, config, wes, force)
+    logging_setup(analysis_dir, project)
+    log_setup_info(project, pipeline, wes, force, analysis_dir, stage_dir, results_dir, config)
+    logging.info(f"Starting staging for project {project}")
+
+    stager = Stager(
+        project, pipeline, analysis_dir, results_dir, stage_dir, config, wes, force
+    )
     exit_code = stager.run()
+    logging.info(f"Staging finished with exit code: {exit_code}")
     exit(exit_code)
+
 
 if __name__ == "__main__":
     main()
